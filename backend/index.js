f4ff0289405998015797900430de839c2b8fe18b
ameliadewi19 <ameliadewi1929@gmail.com>
2023-09-24 10:42:48 +0700

    add wbm

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
-const bookRoute = require("./routes/BookRoute.js");
+const wbm = require('wbm');
 
-const app = express();
-
-app.use(bookRoute);
-
-app.listen(5000, () => console.log('Server Up and Running...'));
+wbm.start().then(async () => {
+    const contacts = [
+        { phone: '+6283150955902', name: 'Amelia', age: 21 },
+        { phone: '+6285157646462', name: 'Dafa', age: 20 }
+    ];
+    const message = 'Hi {{name}}, your age is {{age}}';
+    await wbm.send(contacts, message);
+    await wbm.end();
+}).catch(err => console.log(err));

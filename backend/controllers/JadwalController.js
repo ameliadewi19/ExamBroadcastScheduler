51067beb8dfbb8ece33ebc0e32c9f946aa17a3c7
dafanf <dafa.nurul.tif421@polban.ac.id>
2023-09-24 18:37:30 +0700

    adding get endpoint for jadwal

---
+++ backend/controllers/JadwalController.js
@@ -0,0 +1,82 @@
+const Ujian = require("../models/JadwalModel.js");
+
+const getUjian = async (req, res) => {
+  try {
+    const response = await Ujian.findAll();
+    res.status(200).json(response);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Gagal mendapatkan data ujian." });
+  }
+};
+
+const getUjianById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const response = await Ujian.findOne({
+      where: {
+        id_ujian: id,
+      },
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Gagal mendapatkan data ujian." });
+  }
+};
+
+const createUjian = async (req, res) => {
+  try {
+    const newUjian = await Ujian.create(req.body);
+    res.status(201).json(newUjian);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Gagal membuat ujian baru." });
+  }
+};
+
+const updateUjian = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updatedUjian = await Ujian.update(req.body, {
+      where: {
+        id_ujian: id,
+      },
+    });
+    if (updatedUjian[0] === 1) {
+      res.status(200).json({ message: "Ujian berhasil diperbarui." });
+    } else {
+      res.status(404).json({ error: "Ujian tidak ditemukan." });
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Gagal memperbarui ujian." });
+  }
+};
+
+const deleteUjian = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deleted = await Ujian.destroy({
+      where: {
+        id_ujian: id,
+      },
+    });
+    if (deleted) {
+      res.status(204).send();
+    } else {
+      res.status(404).json({ error: "Ujian tidak ditemukan." });
+    }
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Gagal menghapus ujian." });
+  }
+};
+
+module.exports = {
+  getUjian,
+  getUjianById,
+  createUjian,
+  updateUjian,
+  deleteUjian,
+};
---
+++ backend/models/JadwalModel.js
@@ -0,0 +1,72 @@
+// import Dosen from "DosenModel.js";
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../config/Database.js");
+const Dosen = require("./DosenModel.js"); // Sesuaikan dengan path Anda
+const JadwalUjian = sequelize.define("JadwalUjian", {
+    id_ujian: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    id_dosen: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Dosen, // Mengacu pada model Dosen
+            key: "id_dosen", // Kolom kunci di model Dosen
+        },
+    },
+    tanggal_ujian: {
+        type: DataTypes.DATE,
+        allowNull: false,
+    },
+    waktu_mulai: {
+        type: DataTypes.TIME,
+        allowNull: false,
+    },
+    waktu_selesai: {
+        type: DataTypes.TIME,
+        allowNull: false,
+    },
+    nama_matakuliah: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+    },
+    jenis_matakuliah: {
+        type: DataTypes.CHAR,
+        allowNull: false,
+    },
+    kelas: {
+        type: DataTypes.STRING(20),
+        allowNull: false,
+    },
+    ruangan: {
+        type: DataTypes.STRING(6),
+        allowNull: false,
+    },
+    id_pengawas: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Dosen, // Mengacu pada model Dosen
+            key: "id_dosen", // Kolom kunci di model Dosen
+        },
+    },
+}, {
+    freezeTableName: true,
+    timestamps: false,
+});
+
+// // Definisikan asosiasi antara Ujian dan Dosen di sini jika perlu
+// JadwalUjian.belongsTo(Dosen, {
+//     foreignKey: 'id_dosen',
+//     as: 'dosen', // Alias for the association
+//   });
+  
+// // Misalnya, jika Anda ingin mengaitkan Dosen dengan Ujian, Anda dapat melakukannya di sini.
+
+(async () => {
+    await sequelize.sync();
+})();
+
+module.exports = JadwalUjian;

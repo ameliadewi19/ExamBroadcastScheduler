0caf35559015f3b4cda2f6d087077fb82954c96d
salsabilamp3 <salsabilamp3@gmail.com>
2023-09-24 13:02:48 +0700

    add sendReminder

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const dosenRoute = require("./routes/DosenRoute.js");
+const reminderRoute = require("./routes/ReminderRoute.js");
 
 const app = express();
 
 app.use(dosenRoute);
+
+app.use(reminderRoute);
 
 app.listen(5000, () => console.log('Server Up and Running...'));

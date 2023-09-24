4d118a0e7eb03e8f4c7c640315101036cbaaf81c
ZackyFaishal <zacky.faishal.tif421@polban.ac.id>
2023-09-24 13:32:49 +0700

    Update crud Dosen

--- backend/models/DosenModel.js
+++ backend/models/DosenModel.js
@@ -1,39 +1,39 @@
-const { Sequelize, DataTypes } = require("sequelize");
+    const { Sequelize, DataTypes } = require("sequelize");
 
-const sequelize = require("../config/Database.js");
+    const sequelize = require("../config/Database.js");
 
-const Dosen = sequelize.define("Dosen", {
-    id_dosen: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    nip: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    nidn: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    nama: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    no_whatsapp: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-}, {
-    freezeTableName: true, // This option prevents Sequelize from pluralizing the table name
-    timestamps: false, // Include createdAt and updatedAt columns
-});
+    const Dosen = sequelize.define("Dosen", {
+        id_dosen: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        nip: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        nidn: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        nama: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        no_whatsapp: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    }, {
+        freezeTableName: true, // This option prevents Sequelize from pluralizing the table name
+        timestamps: false, // Include createdAt and updatedAt columns
+    });
 
-// Define any associations, hooks, or additional model methods here
+    // Define any associations, hooks, or additional model methods here
 
-// Sync the model with the database
-(async () => {
-    await sequelize.sync();
-})();
+    // Sync the model with the database
+    (async () => {
+        await sequelize.sync();
+    })();
 
-module.exports = Dosen;
+    module.exports = Dosen;

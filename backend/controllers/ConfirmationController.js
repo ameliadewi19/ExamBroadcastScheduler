b0c5ee75105d916088b84b4929777bd6889b3c01
ameliadewi19 <ameliadewi1929@gmail.com>
2023-09-24 12:46:17 +0700

    edit confirmation

---
+++ backend/controllers/ConfirmationController.js
@@ -0,0 +1,33 @@
+const wbm = require('wbm');
+const Dosen = require("../models/DosenModel.js"); // Sesuaikan dengan path yang benar
+
+const sendConfirmation = async (req, res) => {
+    try {
+        await wbm.start({ showBrowser: true });
+
+        const dosens = await Dosen.findAll();
+
+        const contacts = dosens.map(dosen => ({
+            phone: dosen.no_whatsapp,
+            name: dosen.nama,
+        }));
+
+        const message = 'Hi {{name}}';
+
+        for (const contact of contacts) {
+            await wbm.send([contact], message);
+            const timeoutMillis = 30000; // Sesuaikan durasi timeout sesuai kebutuhan (dalam milidetik)
+            await new Promise(resolve => setTimeout(resolve, timeoutMillis));
+        }
+
+        await wbm.end();
+        res.status(200).json({ message: 'Messages sent successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+module.exports = {
+    sendConfirmation
+};
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const dosenRoute = require("./routes/DosenRoute.js");
+const confirmationRoute = require('./routes/ConfirmationRoute.js');
 
 const app = express();
 
 app.use(dosenRoute);
+app.use(confirmationRoute);
 
 app.listen(5000, () => console.log('Server Up and Running...'));

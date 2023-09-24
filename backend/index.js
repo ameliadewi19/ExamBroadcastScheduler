24905904b00b5e97703094d5a7e4e5f162f62a82
ameliadewi19 <ameliadewi1929@gmail.com>
2023-09-24 10:46:16 +0700

    add timeout wbm

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,17 @@
 const wbm = require('wbm');
 
-wbm.start().then(async () => {
+wbm.start({ showBrowser: true }).then(async () => {
     const contacts = [
+        { phone: '+6283150955902', name: 'Amelia', age: 21 },
         { phone: '+6283150955902', name: 'Amelia', age: 21 },
         { phone: '+6285157646462', name: 'Dafa', age: 20 }
     ];
     const message = 'Hi {{name}}, your age is {{age}}';
-    await wbm.send(contacts, message);
+
+        for (const contact of contacts) {
+            await wbm.send([contact], message);
+            const timeoutMillis = 5000; // Adjust the timeout duration as needed (in milliseconds)
+            await new Promise(resolve => setTimeout(resolve, timeoutMillis));
+        }
     await wbm.end();
 }).catch(err => console.log(err));

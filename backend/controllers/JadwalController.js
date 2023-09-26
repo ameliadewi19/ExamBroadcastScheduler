2e0bd81bcb8fe4519c703aa3b3fbbee5680441d3
dafanf <dafa.nurul.tif421@polban.ac.id>
2023-09-26 14:21:58 +0700

    Fixing getUjian endpoint

--- backend/controllers/JadwalController.js
+++ backend/controllers/JadwalController.js
@@ -1,12 +1,41 @@
-const Dosen = require("../models/DosenModel.js");
 const Ujian = require("../models/JadwalModel.js");
-
+const { Sequelize, DataTypes } = require("sequelize");
+const sequelize = require("../config/Database.js");
 const getUjian = async (req, res) => {
   try {
-    const response = await Ujian.findAll({
-      include:Dosen,
-    });
-    res.status(200).json(response);
+    const query = `
+      SELECT
+        jadwal.id_ujian,
+        jadwal.id_dosen,
+        jadwal.id_pengawas,
+        jadwal.tanggal_ujian,
+        jadwal.waktu_mulai,
+        jadwal.waktu_selesai,
+        jadwal.nama_matakuliah,
+        jadwal.jenis_matakuliah,
+        jadwal.kelas,
+        jadwal.ruangan,
+        dosen_ujian.id_dosen AS id_dosen_ujian,
+        dosen_ujian.nip AS nip_dosen_ujian,
+        dosen_ujian.nidn AS nidn_dosen_ujian,
+        dosen_ujian.nama AS nama_dosen_ujian,
+        dosen_ujian.no_whatsapp AS no_whatsapp_dosen_ujian,
+        dosen_pengawas.id_dosen AS id_dosen_pengawas,
+        dosen_pengawas.nip AS nip_dosen_pengawas,
+        dosen_pengawas.nidn AS nidn_dosen_pengawas,
+        dosen_pengawas.nama AS nama_dosen_pengawas,
+        dosen_pengawas.no_whatsapp AS no_whatsapp_dosen_pengawas
+      FROM
+        "JadwalUjian" jadwal
+      JOIN
+        "Dosen" dosen_ujian ON jadwal.id_dosen = dosen_ujian.id_dosen
+      JOIN
+        "Dosen" dosen_pengawas ON jadwal.id_pengawas = dosen_pengawas.id_dosen;
+    `;
+
+    const [results, metadata] = await sequelize.query(query, { type: Sequelize.QueryTypes.SELECT });
+
+    res.status(200).json(results);
   } catch (error) {
     console.error(error.message);
     res.status(500).json({ error: "Gagal mendapatkan data ujian." });

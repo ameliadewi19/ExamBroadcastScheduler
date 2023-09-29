f27fa6ce6c7babff79c0a13f7108570443b1fdf0
ameliadewi19 <ameliadewi1929@gmail.com>
2023-09-29 13:59:12 +0700

    update template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Sidebar from './components/Sidebar.js';
+import Navbar from './components/Navbar.js';
+import Footer from './components/Footer.js';
+
 import Login from './components/Login.js';
 import Confirmation from './components/Confirmation';
+import Dashboard from './components/Dashboard.js';
+import Dosen from './components/Dosen.js';
+import JadwalUjian from './components/JadwalUjian.js';
+import Reminder from './components/Reminder.js';
 
 function App() {
   return (
@@ -12,7 +21,56 @@ function App() {
             <Login />
           </>
         }/>
-
+        <Route path="/dashboard" element={
+          <div className="wrapper">
+            <Sidebar />
+            <div className="main">
+              <Navbar />
+              <Dashboard />
+              <Footer />
+            </div>
+          </div>
+        }/>
+        <Route path="/dosen" element={
+          <div className="wrapper">
+            <Sidebar />
+            <div className="main">
+              <Navbar />
+              <Dosen />
+              <Footer />
+            </div>
+          </div>
+        }/>
+        <Route path="/jadwal-ujian" element={
+          <div className="wrapper">
+            <Sidebar />
+            <div className="main">
+              <Navbar />
+              <JadwalUjian />
+              <Footer />
+            </div>
+          </div>
+        }/>
+        <Route path="/confirmation" element={
+          <div className="wrapper">
+            <Sidebar />
+            <div className="main">
+              <Navbar />
+              <Confirmation />
+              <Footer />
+            </div>
+          </div>
+        }/>
+        <Route path="/reminder" element={
+          <div className="wrapper">
+            <Sidebar />
+            <div className="main">
+              <Navbar />
+              <Reminder />
+              <Footer />
+            </div>
+          </div>
+        }/>
       </Routes>
     </BrowserRouter>
   );

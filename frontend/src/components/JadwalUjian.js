762e194dfdf017922646e061785828430db11a10
Dafa Nurul Fauziansyah <dafa.nurul.tif421@polban.ac.id>
2023-10-03 11:18:34 +0700

    adding download template file button and adding insert from excel file on client side

--- frontend/src/components/JadwalUjian.js
+++ frontend/src/components/JadwalUjian.js
@@ -37,6 +37,35 @@ const JadwalUjian = () => {
     }
   };
 
+  const handleFileUpload = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('excelFile', file);
+
+    try {
+      const response = await axios.post('http://localhost:5000/jadwal-ujian', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      });
+      console.log(response.data);
+      fetchUjianData();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const downloadExcelTemplate = () => {
+    // Define the URL to your Excel template file within the public/template directory
+    const templateFileUrl = '/template/excel-template.xlsx'; // Adjust the path as needed
+  
+    // Create a link element to trigger the download
+    const link = document.createElement('a');
+    link.href = templateFileUrl;
+    link.setAttribute('download', 'excel-template.xlsx'); // Specify the filename
+    link.click();
+  };
+  
   return (
     <main className="content">
       <div className="container-fluid p-0">
@@ -46,10 +75,11 @@ const JadwalUjian = () => {
             <div className="card">
               <div className="card-header">
                 <h5 className="card-title">Tambah Data</h5>
-                <button className="btn btn-primary mt-2">
-                  <i className="align-middle" data-feather="calendar"></i>
-                  <span className="align-middle">Tambah Dosen</span>
-                </button>
+                <input type="file" accept=".xlsx" onChange={handleFileUpload}/>
+                  <button className="btn btn-primary mt-2" onClick={downloadExcelTemplate}>
+                    <i className="align-middle" data-feather="download"></i>
+                    <span className="align-middle">Download Template</span>
+                  </button>
               </div>
               <div className="card-body">
                 <div className="table-responsive">
@@ -83,12 +113,12 @@ const JadwalUjian = () => {
                           <td>{item.nama_dosen_pengawas}</td>
                           <td>
                             <button className="btn btn-primary mt-2" style={{ marginRight: '5px' }}>
-                              <i className="align-middle" data-feather="edit">.</i>
+                              <i className="align-middle" data-feather="edit"></i>
                             </button>
                             <button
                             className="btn btn-danger mt-2" onClick={() => handleDelete(item.id_ujian)} // Pass the id to the handler
                             >
-                            <i className="align-middle" data-feather="trash">.</i>
+                            <i className="align-middle" data-feather="trash"></i>
                           </button>
                           </td>
                         </tr>

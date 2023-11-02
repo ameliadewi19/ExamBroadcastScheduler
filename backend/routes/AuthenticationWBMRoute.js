bf2a846bdfa35f6f2a7de555bf4628b6a53936a0
ameliadewi19 <ameliadewi1929@gmail.com>
2023-11-02 15:09:16 +0700

    delete

--- backend/routes/AuthenticationWBMRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { 
-    generateQRCode,
-} = require('../controllers/AuthenticationWBMController');
-
-router.get('/generate-qr', generateQRCode);
-
-module.exports = router;
